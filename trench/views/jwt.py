0acb86b41ecb3418239f93000eb4fb24c2c60c5a
Cl0ud-l3ss <aji.khan88@gmail.com>
2023-02-14 16:28:15 +0000

    added remember be for jwt referesh token

--- trench/views/jwt.py
+++ trench/views/jwt.py
@@ -2,15 +2,21 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from trench.views import MFAFirstStepMixin, MFASecondStepMixin, MFAStepMixin, User
+from datetime import datetime, timedelta
 
 
 class MFAJWTView(MFAStepMixin):
+    
     def _successful_authentication_response(self, user: User) -> Response:
         token = RefreshToken.for_user(user=user)
+        remember = self.request.data.get('remember', 1)
+        token['exp'] = datetime.utcnow() + timedelta(days=int(remember))
         return Response(data={"refresh": str(token), "access": str(token.access_token)})
 
     def _remembered_authentication_response(self, user: User) -> Response:
         token = RefreshToken.for_user(user=user)
+        remember = self.request.data.get('remember', 1)
+        token['exp'] = datetime.utcnow() + timedelta(days=int(remember))
         return Response(data={"refresh": str(token), "access": str(token.access_token), 'method': 'remembered'})
 
 
